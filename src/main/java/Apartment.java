1584ffb7b9b4b3037ab5238112f276e8af45532c
IT24103815 <it24103815@my.sliit.lk>
2025-05-16 11:27:47 +0530

    Apartment.java

--- src/main/java/Apartment.java
+++ src/main/java/Apartment.java
@@ -3,22 +3,20 @@ public class Apartment extends Property {
     private int numberOfRooms;
     private boolean hasElevator;
 
-    public Apartment(String propertyId, String location, double size,
-                     double price, String description, String ownerName,
-                     String ownerContact, int floorNumber, int numberOfRooms,
-                     boolean hasElevator) {
-        super(propertyId, location, size, price, description, ownerName, ownerContact);
-        setPropertyType("Apartment");
+    public Apartment(String propertyId, String location, double landSize, double price, String description, String ownerName, String ownerContact, int floorNumber, int numberOfRooms, boolean hasElevator) {
+        super(propertyId, location, landSize, price, description, ownerName, ownerContact);
         this.floorNumber = floorNumber;
         this.numberOfRooms = numberOfRooms;
         this.hasElevator = hasElevator;
     }
 
     public int getFloorNumber() {
+
         return floorNumber;
     }
 
     public void setFloorNumber(int floorNumber) {
+
         this.floorNumber = floorNumber;
     }
 
@@ -27,24 +25,19 @@ public class Apartment extends Property {
     }
 
     public void setNumberOfRooms(int numberOfRooms) {
+
         this.numberOfRooms = numberOfRooms;
     }
 
     public boolean hasElevator() {
+
         return hasElevator;
     }
 
     public void setHasElevator(boolean hasElevator) {
+
         this.hasElevator = hasElevator;
     }
 
-    @Override
-    public void displayDetails() {
-        System.out.println("\n==== Apartment Details ====");
-        super.displayDetails();
-        System.out.println("Size: " + getSize() + " sq.m");
-        System.out.println("Floor Number: " + floorNumber);
-        System.out.println("Number of Rooms: " + numberOfRooms);
-        System.out.println("Has Elevator: " + (hasElevator ? "Yes" : "No"));
-    }
+
 }
